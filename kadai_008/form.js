b4744df33522d838d0bdcc32e4061c8e1f51367c
THET HTUN <87650163+thethtunnn@users.noreply.github.com>
2024-03-10 17:44:18 +0900

    kadai_008/form

---
+++ kadai_008/form.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function() {
+    // Handle click event on the button with class 'btn'
+    $('.btn').on('click', function() {
+        // Display "You clicked!" in the text box with class 'text-box'
+        $('.text-box').val('クリックしました！');
+
+       
+    });
+});
