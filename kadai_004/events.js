f92f171fde61af4cd882dd651814100435319ae5
THET HTUN <87650163+thethtunnn@users.noreply.github.com>
2024-06-06 16:23:16 +0900

    Delete kadai_004 directory
    cc

--- kadai_004/events.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-        console.log("loadイベントが発生しました");
-
-        // $(window)
-        $(document).on('scroll', function() {
-            console.log("scrollイベントが発生しました");
-           
-        });
-    });
